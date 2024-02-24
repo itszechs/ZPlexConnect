470ec3d2cf3159aac22aea0758903e19675d1351
zechs <vijay.vky2002@gmail.com>
2024-02-24 19:27:42 +0530

    added helper get/head routes

--- src/main/kotlin/routes/RemotePlayRoute.kt
+++ src/main/kotlin/routes/RemotePlayRoute.kt
@@ -5,12 +5,20 @@ import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
+import io.ktor.server.routing.get
+import io.ktor.server.routing.head
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import zechs.zplex.connect.models.RemotePlay
 
 fun Application.remotePlayRoute() {
     routing {
+        get("/remote-play") {
+            call.respond("Remote play route is working!")
+        }
+        head("/remote-play") {
+            call.respond(HttpStatusCode.OK)
+        }
         post("/remote-play") {
             val remotePlay = call.receive<RemotePlay>()
             println("Remote play: $remotePlay")
